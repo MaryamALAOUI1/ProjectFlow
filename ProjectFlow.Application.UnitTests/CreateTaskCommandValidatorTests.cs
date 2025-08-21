b99a40ef9272944efe6439240e95f58f899c2352
maryamalaoui <mery.4190@gmail.com>
2025-08-21 11:47:30 +0100

    Complete unit test suite for application validators

---
+++ ProjectFlow.Application.UnitTests/CreateTaskCommandValidatorTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentValidation.TestHelper;
+using Microsoft.EntityFrameworkCore;
+using ProjectFlow.Application.Tasks;
+using ProjectFlow.Domain;
+using ProjectFlow.Infrastructure;
+using Xunit;
+
+namespace ProjectFlow.Application.UnitTests;
+
+public class CreateTaskCommandValidatorTests
+{
+    private readonly ProjectDbContext _context;
+    private readonly CreateTaskCommandValidator _validator;
+
+    public CreateTaskCommandValidatorTests()
+    {
+        var options = new DbContextOptionsBuilder<ProjectDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        _context = new ProjectDbContext(options);
+        _validator = new CreateTaskCommandValidator(_context);
+    }
+
+    [Fact]
+    public async Task Should_Have_Error_When_ProjectId_Does_Not_Exist()
+    {
+        
+        var command = new CreateTaskCommand { Title = "Test Task", ProjectId = 999 };
+        var result = await _validator.TestValidateAsync(command);
+        result.ShouldHaveValidationErrorFor(x => x.ProjectId)
+              .WithErrorMessage("The specified project does not exist.");
+    }
+
+    [Fact]
+    public async Task Should_Not_Have_Error_When_ProjectId_Exists()
+    {
+        var project = new Project { Name = "Existing Project" };
+        _context.Projects.Add(project);
+        await _context.SaveChangesAsync();
+        var command = new CreateTaskCommand { Title = "Test Task", ProjectId = project.Id };
+        var result = await _validator.TestValidateAsync(command);
+        result.ShouldNotHaveValidationErrorFor(x => x.ProjectId);
+    }
+}
---
+++ ProjectFlow.Application.UnitTests/UpdateProjectCommandValidatorTestscs.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation.TestHelper;
+using Microsoft.EntityFrameworkCore;
+using ProjectFlow.Application.Projects;
+using ProjectFlow.Domain;
+using ProjectFlow.Infrastructure;
+using Xunit;
+
+namespace ProjectFlow.Application.UnitTests;
+
+public class UpdateProjectCommandValidatorTests
+{
+    private readonly ProjectDbContext _context;
+    private readonly UpdateProjectCommandValidator _validator;
+
+    public UpdateProjectCommandValidatorTests()
+    {
+        var options = new DbContextOptionsBuilder<ProjectDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        _context = new ProjectDbContext(options);
+        _validator = new UpdateProjectCommandValidator(_context);
+    }
+
+    [Fact]
+    public async Task Should_Have_Error_When_Name_Is_Duplicate_Of_Another_Project()
+    {
+        
+        var project1 = new Project { Name = "Project One" };
+        var project2 = new Project { Name = "Project Two" }; 
+        _context.Projects.AddRange(project1, project2);
+        await _context.SaveChangesAsync();
+
+        var command = new UpdateProjectCommand { Id = project1.Id, Name = "Project Two" };
+        var result = await _validator.TestValidateAsync(command);
+        result.ShouldHaveValidationErrorFor(x => x)
+              .WithErrorMessage("A project with this name already exists.");
+    }
+
+    [Fact]
+    public async Task Should_Not_Have_Error_When_Name_Is_Unchanged()
+    {
+        
+        var project1 = new Project { Name = "Project One" };
+        _context.Projects.Add(project1);
+        await _context.SaveChangesAsync();
+
+        var command = new UpdateProjectCommand { Id = project1.Id, Name = "Project One" };
+
+        var result = await _validator.TestValidateAsync(command);
+
+        result.ShouldNotHaveValidationErrorFor(x => x);
+    }
+}
